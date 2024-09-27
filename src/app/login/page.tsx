d1032210b969b251e0e461667026dd2b86f0ea9a
Annas <lemoll.qyu@gmail.com>
2024-09-27 22:56:18 +0700

    nangkep input login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,35 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
+
 const Login = () => {
+
+    const [ emailPhone, setEmailPhone ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+
+    const handleSubmit = (e:any) => {
+      
+
+        const data = {emailPhone, password};
+
+        console.log(data);
+    }
+
+
+
     return (
     <>
         <div
             className="auth page flex flex-col gap-12 font-nunito bottom-16 absolute left-1/2 -translate-x-1/2"
         >
             <div className="w-32 h-32 bg-gray-300 rounded-full mx-auto"></div>
-            <form className="flex flex-col gap-4">
+            <form action={handleSubmit} className="flex flex-col gap-4">
             <div className="border w-[312px] h-[53px] rounded-lg px-2 pt-4 bg-[#f1f1f1] relative">
                 <span className="absolute text-xs left-2 top-0">Nomor ponsel atau email</span>
                 <input
-                v-model="phoneOrEmail"
+                value={emailPhone}
+                onChange={(e) => setEmailPhone(e.target.value)}
                 type="text"
                 className="font-bold w-full outline-none h-full bg-[#f1f1f1]"
                 />
@@ -19,7 +38,8 @@ const Login = () => {
             <div className="border w-[312px] h-[53px] rounded-lg px-2 pt-4 bg-[#f1f1f1] relative">
                 <span className="absolute text-xs left-2 top-0">Password</span>
                 <input
-                v-model="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 type="password"
                 className="font-bold w-full h-full bg-[#f1f1f1] outline-none"
                 />
